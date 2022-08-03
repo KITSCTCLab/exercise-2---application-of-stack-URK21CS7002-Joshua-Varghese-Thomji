074915045e4ad68056ed809815182d5d640f0374
Joshua-Varghese-KITS <109643810+URK21CS7002-Joshua-Varghese-Thomji@users.noreply.github.com>
2022-08-03 11:18:49 +0530

    Updated expression evaluator

--- Main.py
+++ Main.py
@@ -1,103 +1,115 @@
 class Evaluate:
-  """This class validates and evaluate postfix expression.
+    """This class validates and evaluate postfix expression.
   Attributes:
       top: An integer which denotes the index of the element at the top of the stack currently.
       size_of_stack: An integer which represents the size of stack.
       stack: A List which acts as a Stack.
   """
+
     # Write your code here
 
-
-  def __init__(self, size):
-    """Inits Evaluate with top, size_of_stack and stack.
+    def __init__(self, size):
+        """Inits Evaluate with top, size_of_stack and stack.
     Arguments:
       size_of_stack: An integer to set the size of stack.
     """
-    self.top = -1
-    self.size_of_stack = size
-    self.stack = []
+        self.top = -1
+        self.size_of_stack = size
+        self.stack = []
 
-
-  def isEmpty(self):
-    """
+    def isEmpty(self):
+        """
     Check whether the stack is empty.
     Returns:
       True if it is empty, else returns False.
     """
-    if self.top == -1:
-      return True
-    else:
-      return False
-  
-  def isFull(self):
-    """
+        if self.top == -1:
+            return True
+        else:
+            return False
+
+    def isFull(self):
+        """
     Check whether the stack is full.
     Returns:
       True if it is full, else returns False.
     """
-    if self.top == self.size - 1:
-      return True
-    else:
-      return False
+        if self.top == self.size_of_stack - 1:
+            return True
+        else:
+            return False
 
-
-  def pop(self):
-    """
+    def pop(self):
+        """
     Do pop operation if the stack is not empty.
     Returns:
       The data which is popped out if the stack is not empty.
     """
-    if not(self.isEmpty()):
-      pop = self.stack.pop()
-      self.top -= 1
-      return self.stack[pop]
+        if not (self.isEmpty()):
+            pop = self.stack.pop()
+            self.top -= 1
+            return pop
 
-
-  def push(self, operand):
-    """
+    def push(self, operand):
+        """
     Push the operand to stack if the stack is not full.
     Arguments:
       operand: The operand to be pushed.
     """
-    if not(self.isFull()):
-           self.top += 1
-           self.stack.append(operand)
-           return
+        if not (self.isFull()):
+            self.top += 1
+            self.stack.append(operand)
 
-  def validate_postfix_expression(self, expression):           
-    """
+    def validate_postfix_expression(self, expression):
+        """
     Check whether the expression is a valid postfix expression.
     Arguments:
       expression: A String which represents the expression to be validated.
     Returns:
       True if the expression is valid, else returns False.
     """
-    exp_list = expression.split()
-    for i in exp_list:
-      if len(i) != 1:
-        return False
-      elif i not in "0123456789+-*/^":
-        return False
-      else:
-        return True           
+        exp_list = expression.split()
+        for i in exp_list:
+            if len(i) != 1:
+                return False
+            elif i not in "0123456789+-*/^":
+                return False
+            else:
+                return True
 
-
-  def evaluate_postfix_expression(self, expression):
-    """
+    def evaluate_postfix_expression(self, expression):
+        """
     Evaluate the postfix expression
     Arguments:
-      expression: A String which represents the the expression to be evaluated
+      expression: A String which represents the expression to be evaluated
     Returns:
       The result of evaluated postfix expression.
     """
-    # Write your code here
+        exp_list = expression.split()
+        for i in exp_list:
+            if i in "0123456789":
+                self.push(i)
+            else:
+                a = int(self.pop())
+                b = int(self.pop())
+                if i == "+":
+                    self.push(b + a)
+                elif i == "-":
+                    self.push(b - a)
+                elif i == "*":
+                    self.push(b * a)
+                elif i == "/":
+                    self.push(b / a)
+                elif i == "^":
+                    self.push(b ** a)
+
+        print(self.stack[0])
 
 
-# Do not change the following code
 postfix_expression = input()  # Read postfix expression
 tokens = postfix_expression.split()
 evaluate = Evaluate(len(tokens))
-if evaluate.validate_postfix_expression(tokens):
-    print(evaluate.evaluate_postfix_expression(tokens))
+if evaluate.validate_postfix_expression(postfix_expression):
+    print(evaluate.evaluate_postfix_expression(postfix_expression))
 else:
     print('Invalid postfix expression')
